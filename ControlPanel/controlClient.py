9cb181da837abf3991d5c85e0b90a70b85a624d4
martingaw11 <martingaw11@gmail.com>
2023-11-07 20:23:53 -0600

    send_command for client

--- ControlPanel/controlClient.py
+++ ControlPanel/controlClient.py
@@ -13,4 +13,17 @@ class Client:
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((host, port))
 
-    
+    def send_command(self, command):
+        """Send a command to the server
+
+        Args:
+            command (str): The command to be sent to the server
+
+        """
+        # Working with Java server, so newline character is needed for command processing
+        command += '\n'
+        self.client_socket.send(command.encode('utf-8'))
+        data = self.client_socket.recv(1024)
+        # Terminal will print the feedback from the server (for debugging purposes)
+        print('Received from server: ' + data.decode('utf-8').rstrip())
+
