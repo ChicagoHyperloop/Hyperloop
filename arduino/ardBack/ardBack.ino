0d48cfb51f012503cd6caaaf181ca52e52ebc9f1
root <chicagohyperloop@gmail.com>
2024-01-23 21:31:34 +0000

    java somn somn

--- arduino/ardBack/ardBack.ino
+++ arduino/ardBack/ardBack.ino
@@ -1,37 +1,25 @@
-/*
-  Blink
 
-  Turns an LED on for one second, then off for one second, repeatedly.
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);  // Initialize Serial communication
+  pinMode(13, OUTPUT);  // Set Pin 13 as an output
+  Serial.flush();
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(2000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(2000);                      // wait for a second
+  if (Serial.available() > 0) {  // Check if data is available to read
+    String receivedString = Serial.readStringUntil('\n');  // Read the incoming data until a newline character is received
+    ///receivedString.trim();
+    
+    
+    if (receivedString == "START") {  // Check if the received string is "START"
+      digitalWrite(13, HIGH);  // Turn on Pin 13
+      
+    } else if (receivedString == "STOP") {
+      digitalWrite(13, LOW);  // Turn off Pin 13
+    }
+  }
+  
+  // Add any other code you want to run in the loop here
 }
+
