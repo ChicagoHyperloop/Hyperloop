f80068de8e9147fd6a990a3f280dfdcf2fce714e
FFFUUUUUCCCKKK instant Rice <zain@accucalc.com>
2024-01-25 11:47:34 -0600

    bloop

--- mainProc/ArdCommTest/src/main/java/org/example/Main.java
+++ mainProc/ArdCommTest/src/main/java/org/example/Main.java
@@ -23,33 +23,33 @@ public class Main {
             System.out.println(sp.getDescriptivePortName() + " - " + sp.getSystemPortPath());
         }
 
-        ardPort1 = allports[2];    // /dev.cu.usbmodem1101 on Pranav's computer
+        ardPort1 = allports[0];    // /dev.cu.usbmodem1101 on Pranav's computer
         ardPort1.openPort();
+
 
         InputStream ardin = ardPort1.getInputStream();
         OutputStream ardout = ardPort1.getOutputStream();
 
-
         System.out.println("Connection to Arduino 1 established");
+
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
             System.out.println("Press 1 to start and 0 to stop");
             String comm = scanner.nextLine();
             System.out.println(comm);
             //comm.trim();
+
             if (Objects.equals(comm, "1")) {
                 ardout.write("START\n".getBytes());
                 System.out.println("Thing is on");
+
             } else if (Objects.equals(comm, "0")) {
-                ardout.write("STOP\n".getBytes());
+
+	       ardout.write("STOP\n".getBytes());
                 System.out.println("Thing is off");
+
             }
-
         }
-
-
-
-
-
     }
 }
---
+++ mainProc/ArdCommTest/src/main/java/org/example/arduino.java
@@ -0,0 +1,18 @@
+
+import com.fazecast.jSerialComm.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Objects;
+import java.util.Scanner;
+/*
+public class arduino {
+
+/*	public arduino() {}
+
+	public read() {}
+
+	public write() {}
+*/
+//}
+
